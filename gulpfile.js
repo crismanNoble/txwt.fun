f8159c0d0f4d7aaa5c3290f81121da99abdf78c8
Cris Noble <cris@nobleintentstudio.com>
2023-08-20 13:57:01 -0700

    new stuff

--- gulpfile.js
+++ gulpfile.js
@@ -13,14 +13,40 @@ gulp.task('sass', function () {
       cascade: false
     }))
     .pipe(cssmin())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('docs'));
 });
 
 gulp.task('js', function() {
   return gulp.src('./src/**/*.js')
     .pipe(minify())
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('docs'))
 });
+
+gulp.task('replace', function(){
+  var image_list_raw = require('./data/image_list.json');
+  var image_list_all = [];
+  for (let img of image_list_raw) {
+    // console.log(img);
+    var i = img.split('/docs/')[1];
+    i = './'+i;
+    image_list_all.push(i);
+  }
+  var image_list = JSON.stringify(image_list_all);
+  var people_lookup = JSON.stringify(require('./data/people_lookup.json'));
+
+  return gulp.src(['index.html'])
+    .pipe(replace('{{image_list}}', image_list.toString()))
+    .pipe(replace('{{people_lookup}}', people_lookup.toString()))
+    .pipe(gulp.dest('docs/'));
+});
+
+gulp.task('image_crawl', function(){
+  return gulp
+    .src(['./docs/images/**/*.800.jpg','./docs/images/**/*.mov','./docs/images/**/*.mp4','./docs/images/**/*.MP4'])
+    .pipe(require('gulp-filelist')('image_list.json', { absolute: true }))
+    .pipe(gulp.dest('data'))
+});
+
  
 gulp.task('sass:watch', gulp.series('sass', function (done) {
   gulp.watch('./src/**/*.scss', gulp.series('sass'));
@@ -32,7 +58,12 @@ gulp.task('js:watch', gulp.series('js', function (done) {
   done();
 }));
 
-gulp.task('watch',gulp.parallel('sass:watch', 'js:watch', function(done){
+gulp.task('file:watch', gulp.series('replace', function (done) {
+  gulp.watch('./index.html', gulp.series('replace'));
+  done();
+}));
+
+gulp.task('watch',gulp.parallel('sass:watch', 'js:watch','file:watch', function(done){
   done();
 }));
 
@@ -50,27 +81,9 @@ gulp.task('exclude',gulp.parallel('exclude:node_modules', 'exclude:git_folder', 
   done();
 }));
 
+
 gulp.task('default', gulp.series('watch', 'exclude', function(done) {    
   done();
 }));
 
-function replaceTemplate() {
-  return src(['file.txt'])
-    .pipe(replace('bar', 'foo'))
-    .pipe(dest('build/'));
-};
-gulp.task('replace', function(){
-  var image_list = JSON.stringify(require('./data/image_list.json'));
-  var people_lookup = JSON.stringify(require('./data/people_lookup.json'));
-  return gulp.src(['index.html'])
-    .pipe(replace('{{image_list}}', image_list.toString()))
-    .pipe(replace('{{people_lookup}}', people_lookup.toString()))
-    .pipe(gulp.dest('docs/'));
-});
 
-gulp.task('image_crawl', function(){
-  return gulp
-    .src('./docs/images/**/*.*')
-    .pipe(require('gulp-filelist')('image_list.json', { absolute: true }))
-    .pipe(gulp.dest('data'))
-});
